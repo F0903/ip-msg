4f3e0f7369341fedcf02f984d73ddcd3e6da358e
Frederik P. <frepbh@gmail.com>
2024-10-13 10:48:38 +0200

    finished impl of async lazy cell

--- src-tauri/src/utils/async_lazy_cell.rs
+++ src-tauri/src/utils/async_lazy_cell.rs
@@ -1,4 +1,4 @@
-use std::{future::Future, pin::Pin};
+use std::{cell::UnsafeCell, future::Future, pin::Pin};
 
 type Result<T, E> = std::result::Result<T, E>;
 
@@ -8,38 +8,55 @@ enum State<T, F> {
 }
 
 pub struct AsyncLazyCell<T, E> {
-    state: State<T, fn() -> Pin<Box<dyn Future<Output = Result<T, E>> + Send>>>,
+    state: UnsafeCell<State<T, fn() -> Pin<Box<dyn Future<Output = Result<T, E>> + Send>>>>,
 }
 
 impl<T, E> AsyncLazyCell<T, E> {
     pub const fn new(getter: fn() -> Pin<Box<dyn Future<Output = Result<T, E>> + Send>>) -> Self {
         Self {
-            state: State::Uninit(getter),
+            state: UnsafeCell::new(State::Uninit(getter)),
         }
     }
 
     pub const fn with_value(value: T) -> Self {
         Self {
-            state: State::Init(value),
+            state: UnsafeCell::new(State::Init(value)),
         }
     }
 
-    fn init(&self) {
-        //TODO
+    async fn init(&self) -> Result<&T, E> {
+        let state = unsafe { &*self.state.get() };
+        match state {
+            State::Uninit(f) => {
+                let val = f().await?;
+
+                let state_mut = unsafe { &mut *self.state.get() };
+                *state_mut = State::Init(val);
+
+                let val_ref = unsafe { &*self.state.get() };
+                match val_ref {
+                    State::Init(x) => Ok(x),
+                    State::Uninit(_) => unreachable!(),
+                }
+            }
+            State::Init(_) => unreachable!(),
+        }
     }
 
     pub async fn get<'a>(&'a self) -> Result<&'a T, E>
     where
         T: 'a,
     {
-        match &self.state {
+        let state = unsafe { &*self.state.get() };
+        match state {
             State::Init(x) => Ok(x),
             State::Uninit(_) => {
-                self.init();
-                Ok(match &self.state {
-                    State::Init(x) => x,
+                self.init().await?;
+                let new_state = unsafe { &*self.state.get() };
+                match new_state {
+                    State::Init(x) => Ok(x),
                     _ => unreachable!(),
-                })
+                }
             }
         }
     }
